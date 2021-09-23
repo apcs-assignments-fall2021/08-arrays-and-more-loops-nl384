4254ed490dad96d1fb600ede5f259b6914bba360
Nathan Lin <nlin@athenian.org>
2021-09-23 09:19:24 -0700

    draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,22 +5,36 @@ public class MyMain {
     // Rotates all values in an array to the left
     // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
     public static int[] rotateLeft(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int temp = arr[0];
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        arr[arr.length-1] = temp;
+        return arr;
     }
 
     // Rotates all values in an array to the right
     // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
     public static int[] rotateRight(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int temp = arr[arr.length-1];
+        for (int i = arr.length-1; i > 0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+        return arr;
     }
 
     // Replaces all duplicate values with -1
     // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
     public static int[] duplicateReplacer(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[j] = -1;
+                }
+            }
+        }
+        return arr;
     }
 
 
